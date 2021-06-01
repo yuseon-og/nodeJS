78294e470a1cac95429d0f34fde7ac1f968b91be
Yuseon <itsyuseon@gmail.com>
2021-06-01 14:22:12 +0900

    at office

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 const MongoClient = require("mongodb").MongoClient;
 
 let db;
+let id = 1;
 
 MongoClient.connect(
   "mongodb+srv://admin:admin1234@cluster0.gbnj2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -13,12 +15,12 @@ MongoClient.connect(
     }
     db = client.db("todoapp");
 
-    db.collection("post").insertOne(
-      { _id: 2, 이름: "OK", 나이: 35 },
-      (error, result) => {
-        console.log("저장완료");
-      }
-    );
+    // db.collection("post").insertOne(
+    //   {_id: 2, 이름: "OK", 나이: 35},
+    //   (error, result) => {
+    //     console.log("저장완료");
+    //   }
+    // );
 
     app.listen(8080, () => {
       console.log("listening on 8080 okok");
@@ -26,12 +28,25 @@ MongoClient.connect(
   }
 );
 
-app.get("/pet", (request, response) => {
-  response.send("PET 쇼핑입니다.");
+app.post("/add", (req, res) => {
+  res.redirect("/write");
+  db.collection("post").insertOne(
+    {_id: id, 제목: req.body.title, 날짜: req.body.date},
+    (error, result) => {
+      id = id + 1;
+      console.log("저장완료");
+      console.log("next id is" + id);
+    }
+  );
 });
 
-app.get("/beauty", (request, response) => {
-  response.send("beauty 쇼핑입니다.");
+app.get("/list", (req, res) => {
+  db.collection("post")
+    .find()
+    .toArray((error, result) => {
+      console.log(result);
+      res.render("list.ejs", {posts: result});
+    });
 });
 
 app.get("/", (request, response) => {
@@ -45,12 +60,12 @@ app.get("/write", (request, response) => {
 // 어떤 사람이 / add 경로로 POST 요청을 하면...
 // ~~~를 해주세요
 
-app.post("/add", (request, response) => {
-  response.send("complete");
-  console.log(request.body.title);
-  console.log(request.body.date);
-  // DB에 저장해주세요~~~ 추가예정
-});
+// app.post("/add", (request, response) => {
+//   response.send("complete");
+//   console.log(request.body.title);
+//   console.log(request.body.date);
+
+// });
 
 // 어떤 사람이 / add 라는 경로로 post 요청을 하면,
 //     데이터 2개를(날짜, 제목) 보내주는데,
